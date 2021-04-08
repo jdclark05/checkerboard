17ae3074406d01f4008faabb1e9d0b3b68a583a4
James Clark <jdclark05@gmail.com>
2021-04-08 19:23:38 -0400

    fix varibale nomencalature

--- checkerboard.py
+++ checkerboard.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/<int:x>/<int:y>")
 def index(x, y):
-    return render_template("index.html", x=x, y=y)
+    return render_template("index.html", num1=x, num2=y)
 
 if __name__=="__main__":
     app.run(debug=True)
